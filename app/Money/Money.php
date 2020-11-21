183d6115bbd7562a5b202d716d1a00ac30427c6f
harinezumi <cc89mk@gmail.com>
2020-11-22 01:06:08 +0900

    value object

---
+++ app/Money/Money.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+class Money
+{
+    private $amount;
+    private $currency;
+
+    public function __construct($anAmount, Currency $aCurrency)
+    {
+        $this->setAmount($anAmount);
+        $this->setCurrency($aCurrency);
+    }
+
+    private function setAmount($anAmount)
+    {
+        $this->amount = (int)$anAmount;
+
+    }
+
+    private function setCurrency(Currency $aCurrency)
+    {
+        $this->currency = $aCurrency;
+    }
+
+    public function amount()
+    {
+        return $this->amount;
+    }
+
+    public function currency()
+    {
+        return $this->currency;
+    }
+}
